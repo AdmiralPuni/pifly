63aa32a59c1ee0090c6dd27ab675a5478d6fc955
AdmiralPuni <admiralpuni@protonmail.com>
2021-11-16 14:41:29 +0700

    too many

--- mqtt.py
+++ mqtt.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import json
 import mysql.connector
+from tqdm import tqdm
 
 database = mysql.connector.connect(
     host='localhost',
@@ -16,32 +17,39 @@ json_data = {
     'notification' : 23
 }
 
+progress_battery = tqdm(total = 100)
+progress_battery.desc = "Battery-1"
+progress_battery.refresh()
+
 devices = []
 devices.append([])
+topics = ['battery', 'water_level']
 
 devices[0].append('notification')
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("battery2")
-    client.subscribe("battery")
+    #print("Connected with result code "+str(rc))
+    for top in topics:
+        client.subscribe(top)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     realmsg = str(msg.payload)[:-1]
     realmsg = realmsg[2:]
-    print(msg.topic, realmsg)
-    sql = 'UPDATE devices SET battery=' + str(float(realmsg))
-    database_cursor.execute(sql)
-    database.commit()
-    if msg.topic == 'hello':
-        json_data['notification'] = str(realmsg)
-        with open(json_file, 'w') as f:
-            json.dump(json_data, f)
+    #print(msg.topic, realmsg)
+    
+    for top in topics:
+        if top == msg.topic:
+            if top == 'battery':
+                progress_battery.n = (float(realmsg))
+                progress_battery.refresh()
+            sql = 'UPDATE devices SET ' + top + '=' + str(float(realmsg))
+            database_cursor.execute(sql)
+            database.commit()
+            json_data[top] = str(realmsg)
+            with open(json_file, 'w') as f:
+                json.dump(json_data, f)
 
 client = mqtt.Client()
 client.on_connect = on_connect
---
+++ website.py
@@ -0,0 +1,28 @@
+import flask
+import json
+import sql
+
+# Create the application.
+APP = flask.Flask(__name__)
+
+# Create a URL route in our application for "/"
+@APP.route('/')
+def home():
+  return flask.render_template('index.html')
+
+@APP.route('/stats')
+def stats():
+  return flask.render_template('chart.html')
+
+@APP.route('/devices')
+def coindata():
+  data = sql.select_device_id()
+  return json.dumps(data)
+
+@APP.route('/data')
+def data():
+  data = sql.select_all_device()
+  return json.dumps(data)
+
+if __name__ == '__main__':
+  APP.run(debug=True)
